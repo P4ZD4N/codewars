5da9ef8c424573f0654c8a08218fd2b3bcf5ad5b
Wiktor <wiko700@gmail.com>
2023-06-20 16:21:10 +0200

    Added project "create-phone-number"

---
+++ create-phone-number/src/Kata.java
@@ -0,0 +1,19 @@
+public class Kata {
+    public static String createPhoneNumber(int[] numbers) {
+        StringBuilder firstPart = new StringBuilder();
+        StringBuilder secondPart = new StringBuilder();
+        StringBuilder thirdPart = new StringBuilder();
+
+        for (int i = 0; i <= 9; i++) {
+            if (i >= 0 && 2 >= i) {
+                firstPart.append(numbers[i]);
+            } else if (i >= 3 && 5 >= i) {
+                secondPart.append(numbers[i]);
+            } else {
+                thirdPart.append(numbers[i]);
+            }
+        }
+        return "(" + firstPart.toString() + ") " + secondPart.toString() + "-" + thirdPart.toString();
+    }
+    public static void main(String[] args) {}
+}
