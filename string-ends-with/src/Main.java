d421c7e60b5bb230fd7b05925e01fb160038c18b
Wiktor <wiko700@gmail.com>
2023-07-11 12:23:38 +0200

    Added solution of problem "String ends with?"

---
+++ string-ends-with/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Kata.solution("abc", "bc"));
+        System.out.println(Kata.solution("abc", "d"));
+        System.out.println(Kata.solution("Hello World!", "ld!"));
+    }
+}
